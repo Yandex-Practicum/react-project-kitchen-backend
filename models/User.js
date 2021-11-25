47ac3151f6487f5d67305ae939843c838f97a1e8
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-11-25 22:25:27 +0300

    feat: add default avatar link

--- models/User.js
+++ models/User.js
@@ -53,7 +53,7 @@ UserSchema.methods.toProfileJSONFor = function(user){
   return {
     username: this.username,
     bio: this.bio,
-    image: this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
+    image: this.image || 'https://i.imgur.com/0j9huTT.jpeg',
     following: user ? user.isFollowing(this._id) : false
   };
 };
